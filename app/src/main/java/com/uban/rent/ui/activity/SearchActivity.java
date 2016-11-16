e4749f08536f7e3dfda68a022f88860992eccd67
王波 <wangbo@uban.com>
2016-11-16 19:46:45 +0800

    ref

--- app/src/main/java/com/uban/rent/ui/activity/SearchActivity.java
+++ app/src/main/java/com/uban/rent/ui/activity/SearchActivity.java
@@ -2,11 +2,21 @@ package com.uban.rent.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatEditText;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.uban.rent.R;
 import com.uban.rent.base.BaseActivity;
+import com.uban.rent.ui.view.UbanListView;
+import com.uban.rent.ui.view.flowlayout.FlowLayout;
+import com.uban.rent.ui.view.flowlayout.TagAdapter;
+import com.uban.rent.ui.view.flowlayout.TagFlowLayout;
+
+import java.util.Set;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -20,6 +30,12 @@ public class SearchActivity extends BaseActivity {
     AppCompatEditText editSearch;
     @Bind(R.id.activity_search)
     LinearLayout activitySearch;
+    @Bind(R.id.tag_flow_layout)
+    TagFlowLayout tagFlowLayout;
+    @Bind(R.id.search_history)
+    UbanListView searchHistory;
+    private String[] mVals = new String[]
+            {"望京", "国贸CBD", "望京", "西直门", "国贸CBD", "国贸CBD", "国贸CBD", "国贸CBD", "望京", "国贸CBD", "望京", "西直门", "国贸CBD"};
 
     @Override
     protected int getLayoutId() {
@@ -28,7 +44,35 @@ public class SearchActivity extends BaseActivity {
 
     @Override
     protected void afterCreate(Bundle savedInstanceState) {
+        initView();
+    }
 
+    private void initView() {
+       TagAdapter<String> tagAdapter =  new TagAdapter<String>(mVals) {
+            @Override
+            public View getView(FlowLayout parent, int position, String s) {
+                TextView tv = (TextView) getLayoutInflater().inflate(R.layout.item_search_tag_view, tagFlowLayout, false);
+                tv.setText(s);
+                return tv;
+            }
+        };
+        tagFlowLayout.setAdapter(tagAdapter);
+        tagFlowLayout.setOnTagClickListener(new TagFlowLayout.OnTagClickListener() {
+            @Override
+            public boolean onTagClick(View view, int position, FlowLayout parent) {
+                Toast.makeText(mContext, mVals[position], Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        tagFlowLayout.setOnSelectListener(new TagFlowLayout.OnSelectListener() {
+            @Override
+            public void onSelected(Set<Integer> selectPosSet) {
+                Toast.makeText(mContext, selectPosSet.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ArrayAdapter mAdapter = new ArrayAdapter<>(mContext, android.R.layout.simple_list_item_1, mVals);
+        searchHistory.setAdapter(mAdapter);
     }
 
     @Override
