f216fc9e0d05fc7ff876d02d568db2696b097cc4
liyun <liyun@uban.com>
2016-11-18 11:18:38 +0800

    liyun

--- app/src/main/java/com/uban/rent/ui/activity/OrdersDetailActivity.java
+++ app/src/main/java/com/uban/rent/ui/activity/OrdersDetailActivity.java
@@ -1,15 +1,23 @@
 package com.uban.rent.ui.activity;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.uban.rent.R;
 import com.uban.rent.base.BaseActivity;
+
+import butterknife.Bind;
 
 /**
  * 订单详情页
  */
 public class OrdersDetailActivity extends BaseActivity {
-
+    @Bind(R.id.toolbar_content_text)
+    TextView toolbarContentText;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
     @Override
     protected int getLayoutId() {
         return R.layout.activity_orders_detail;
@@ -17,7 +25,17 @@ public class OrdersDetailActivity extends BaseActivity {
 
     @Override
     protected void afterCreate(Bundle savedInstanceState) {
-
+        initView();
+    }
+    private void initView() {
+        toolbarContentText.setText("订单详情");
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
     }
 
 }
