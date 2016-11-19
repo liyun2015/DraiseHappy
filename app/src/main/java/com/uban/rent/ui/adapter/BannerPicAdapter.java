77133ea8998c6b64cd54cae1ecfa79c744cbd13d
王波 <wangbo@uban.com>
2016-11-19 17:56:45 +0800

    ref

--- app/src/main/java/com/uban/rent/ui/adapter/BannerPicAdapter.java
+++ app/src/main/java/com/uban/rent/ui/adapter/BannerPicAdapter.java
@@ -3,13 +3,19 @@ package com.uban.rent.ui.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.nostra13.universalimageloader.core.ImageLoader;
 import com.uban.rent.R;
 import com.uban.rent.ui.activity.ShowPhotosActivity;
 import com.uban.rent.util.ImageLoadUtils;
@@ -57,7 +63,8 @@ public class BannerPicAdapter extends PagerAdapter {
         ImageView imageView = (ImageView) view.findViewById(R.id.image_banner);
         ImageLoadUtils.displayImage(images.get(position),imageView);
 
-        Bitmap newimage = ImageLoader.getInstance().loadImageSync(images.get(position));
+        Bitmap image = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+        Bitmap newimage = getRoundCornerImage(image,50);
         ImageView imageView2 = new ImageView(view.getContext());
         imageView2.setImageBitmap(newimage);
         container.addView(view);
@@ -75,5 +82,18 @@ public class BannerPicAdapter extends PagerAdapter {
             }
         });
         return view;
+    }
+
+    public Bitmap getRoundCornerImage(Bitmap bitmap, int roundPixels) {
+        Bitmap roundConcerImage = Bitmap.createBitmap(bitmap.getWidth(),bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(roundConcerImage);
+        Paint paint = new Paint();
+        Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        RectF rectF = new RectF(rect);
+        paint.setAntiAlias(true);
+        canvas.drawRoundRect(rectF, roundPixels, roundPixels, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, null, rect, paint);
+        return roundConcerImage;
     }
 }
---
+++ app/src/main/java/com/uban/rent/ui/adapter/EquipmentServiceAdapter.java
@@ -0,0 +1,68 @@
+package com.uban.rent.ui.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.uban.rent.R;
+import com.uban.rent.base.UBBaseAdapter;
+import com.uban.rent.util.ImageLoadUtils;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * EquipmentServiceAdapter 设备服务列表item
+ * Created by dawabos on 2016/11/19.
+ * Email dawabo@163.com
+ */
+
+public class EquipmentServiceAdapter extends UBBaseAdapter<String, ListView> {
+    private List<String> listNames;
+    private List<String> listImages;
+    public EquipmentServiceAdapter(Context context, List<String> listNames,List<String> listImages) {
+        super(context, listNames);
+        this.listNames = listNames;
+        this.listImages = listImages;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        String name = listNames.get(i);
+        String image = listImages.get(i);
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = View.inflate(context, R.layout.item_detail_service_equipment, null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.tvServiceEquipmentText.setText(name);
+        ImageLoadUtils.displayImage(image,holder.ivServiceEquipmentImage);
+        return convertView;
+    }
+
+    public void changeData(List<String> list) {
+        this.list = list;
+        notifyDataSetChanged();
+    }
+
+
+    static class ViewHolder {
+        @Bind(R.id.iv_service_equipment_image)
+        ImageView ivServiceEquipmentImage;
+        @Bind(R.id.tv_service_equipment_text)
+        TextView tvServiceEquipmentText;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
