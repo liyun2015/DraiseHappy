0f3d7f8428c5f2d4d5fc71f04596e8c1da3c3b03
liyun <liyun@uban.com>
2016-11-30 11:03:34 +0800

    liyun

--- app/src/main/java/com/uban/rent/wxapi/WXPayEntryActivity.java
+++ app/src/main/java/com/uban/rent/wxapi/WXPayEntryActivity.java
@@ -1,17 +1,19 @@
 package com.uban.rent.wxapi;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
-import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -33,6 +35,7 @@ import com.uban.rent.module.request.RequestPaymentOrder;
 import com.uban.rent.module.request.UnifieOrderBean;
 import com.uban.rent.ui.activity.order.OrdersDetailActivity;
 import com.uban.rent.ui.view.ToastUtil;
+import com.uban.rent.util.CommonUtil;
 import com.uban.rent.util.Constants;
 import com.uban.rent.util.IpUtils;
 
@@ -59,8 +62,6 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
     TextView orderState;
     @Bind(R.id.bottom_view)
     LinearLayout bottomView;
-    @Bind(R.id.activity_order_payment)
-    RelativeLayout activityOrderPayment;
     @Bind(R.id.switch_btn_one)
     CheckBox switchBtnOne;
     @Bind(R.id.switch_btn_two)
@@ -69,6 +70,10 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
     TextView cancelOrderBtn;
     @Bind(R.id.message_remind_str)
     TextView messageRemindStr;
+    @Bind(R.id.darkening_background_layout)
+    LinearLayout darkeningBackgroundLayout;
+    @Bind(R.id.activity_order_payment)
+    RelativeLayout activityOrderPayment;
     private int payType = 1;//1微信，2支付宝
     private String orderNum;
     private int state;
@@ -79,6 +84,8 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
     private int payStatus;
     private int paymentTime;
     private TimeCount time;
+    private static final String[] CANCEL_REASON_STR = new String[]{"我要重新预定", "下错订单", "不需要预定了", "其他"};
+    private LayoutInflater mInflater;
 
     @Override
     protected int getLayoutId() {
@@ -91,6 +98,7 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
         api = WXAPIFactory.createWXAPI(this, Constants.APP_ID);
         api.registerApp(Constants.APP_ID);
         api.handleIntent(getIntent(), this);
+        mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         initView();
     }
 
@@ -190,13 +198,17 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
         orderIntent.setClass(mContext, cls);
         orderIntent.putExtra(OrdersDetailActivity.KEY_ORDER_NUMBER, String.valueOf(resultsBean.getOrderNo()));
         startActivity(orderIntent);
+        if (null != time) {
+            time.cancel();
+        }
+        finish();
     }
 
     @OnClick({R.id.cancel_order_btn, R.id.order_create})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.cancel_order_btn://取消订单
-                cancelShortRentOrder();
+                showCancelPop();
                 break;
             case R.id.order_create://提交订单
                 submitOrder();
@@ -204,6 +216,86 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
             default:
                 break;
         }
+    }
+
+    private TextView cancel_reason_one;
+    private TextView cancel_reason_two;
+    private TextView cancel_reason_three;
+    private TextView cancel_reason_four;
+    private String cancelReason = CANCEL_REASON_STR[0];
+    private PopupWindow cancelPopupWindow;
+
+    private void showCancelPop() {
+        darkeningBackgroundLayout.setVisibility(View.VISIBLE);
+        View cancelView = mInflater.inflate(R.layout.choose_cancel_reason_pop, null);
+        cancel_reason_one = (TextView) cancelView.findViewById(R.id.cancel_reason_one);
+        cancel_reason_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelReason = CANCEL_REASON_STR[0];
+                cancel_reason_one.setTextColor(getResources().getColor(R.color.colorPrimary));
+                cancel_reason_two.setTextColor(getResources().getColor(R.color.colorGrayHint));
+                cancel_reason_three.setTextColor(getResources().getColor(R.color.colorGrayHint));
+                cancel_reason_four.setTextColor(getResources().getColor(R.color.colorGrayHint));
+            }
+        });
+        cancel_reason_two = (TextView) cancelView.findViewById(R.id.cancel_reason_two);
+        cancel_reason_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelReason = CANCEL_REASON_STR[1];
+                cancel_reason_one.setTextColor(getResources().getColor(R.color.colorGrayHint));
+                cancel_reason_two.setTextColor(getResources().getColor(R.color.colorPrimary));
+                cancel_reason_three.setTextColor(getResources().getColor(R.color.colorGrayHint));
+                cancel_reason_four.setTextColor(getResources().getColor(R.color.colorGrayHint));
+            }
+        });
+        cancel_reason_three = (TextView) cancelView.findViewById(R.id.cancel_reason_three);
+        cancel_reason_three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelReason = CANCEL_REASON_STR[2];
+                cancel_reason_one.setTextColor(getResources().getColor(R.color.colorGrayHint));
+                cancel_reason_two.setTextColor(getResources().getColor(R.color.colorGrayHint));
+                cancel_reason_three.setTextColor(getResources().getColor(R.color.colorPrimary));
+                cancel_reason_four.setTextColor(getResources().getColor(R.color.colorGrayHint));
+            }
+        });
+        cancel_reason_four = (TextView) cancelView.findViewById(R.id.cancel_reason_four);
+        cancel_reason_four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelReason = CANCEL_REASON_STR[3];
+                cancel_reason_one.setTextColor(getResources().getColor(R.color.colorGrayHint));
+                cancel_reason_two.setTextColor(getResources().getColor(R.color.colorGrayHint));
+                cancel_reason_three.setTextColor(getResources().getColor(R.color.colorGrayHint));
+                cancel_reason_four.setTextColor(getResources().getColor(R.color.colorPrimary));
+            }
+        });
+        TextView submit_right_btn = (TextView) cancelView.findViewById(R.id.submit_btn);
+        submit_right_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cancelPopupWindow.isShowing()) {
+                    cancelPopupWindow.dismiss();
+                    darkeningBackgroundLayout.setVisibility(View.GONE);
+                }
+                cancelShortRentOrder();
+            }
+        });
+        int screeWidth = getWindowManager().getDefaultDisplay().getWidth();
+        cancelPopupWindow = CommonUtil.ShowBottomPopupWindow(
+                WXPayEntryActivity.this, cancelPopupWindow, cancelView, screeWidth,
+                220, activityOrderPayment);
+        CommonUtil.setPopupWindowListener(new CommonUtil.PopupWindowListener() {
+
+            @Override
+            public void myDissmiss() {
+                cancelPopupWindow = null;
+                darkeningBackgroundLayout.setVisibility(View.GONE);
+            }
+
+        });
     }
 
     private void submitOrder() {
@@ -280,6 +372,7 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
         RequestPaymentOrder requestPaymentOrder = new RequestPaymentOrder();
         requestPaymentOrder.setOrderNo(orderNum);
         requestPaymentOrder.setState(state);
+        requestPaymentOrder.setRefundDesc(cancelReason);
         requestPaymentOrder.setWorkDeskType(workdeskType);
         ServiceFactory.getProvideHttpService().cancelShortRentOrder(requestPaymentOrder)
                 .compose(this.<RequestCreatShortRentOrderBean>bindToLifecycle())
@@ -310,7 +403,6 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
                     public void call(RequestCreatShortRentOrderBean.ResultsBean resultsBean) {
                         //处理返回结果
                         goActivity(OrdersDetailActivity.class, resultsBean);
-                        finish();
                     }
                 }, new Action1<Throwable>() {
                     @Override
@@ -373,6 +465,9 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
 
     private boolean payBooleanSucceed(ResultOrderQueryBean resultOrderQueryBean) {
         if (resultOrderQueryBean.getStatusCode() == Constants.STATUS_CODE_ERROR) {
+            if (null != time) {
+                time.cancel();
+            }
             goActivity(OrdersDetailActivity.class, resultsBean);
             finish();
         }
@@ -397,10 +492,13 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
             queryOrder();
         }
     }
+
     private void StartCountDown(int failureAt) {
+        messageRemindStr.setVisibility(View.VISIBLE);
         time = new TimeCount(failureAt * 1000, 1000);
         time.start();// 开始计时
     }
+
     class TimeCount extends CountDownTimer {
         public TimeCount(long millisInFuture, long countDownInterval) {
             super(millisInFuture, countDownInterval);// 参数依次为总时长,和计时的时间间隔
@@ -409,7 +507,6 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
         @Override
         public void onFinish() {
             //倒计时完毕
-            messageRemindStr.setVisibility(View.GONE);
             messageRemindStr.setVisibility(View.GONE);
         }
 
@@ -423,7 +520,7 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
                 messageRemindStr.setText(hour + "时" + min + "分" + sec + "秒");
             } else {
                 String countTime = (int) millisUntilFinished / (1000 * 60) + "分" + (millisUntilFinished / 1000) % 60 + "秒";
-                messageRemindStr.setText(Html.fromHtml("请在 <font color='#FF5254' >" + countTime + "</font>内完成支付，晚了就没有了哦！"));
+                messageRemindStr.setText(Html.fromHtml("请在 <font color='#FF5254' >" + countTime + "</font>内完成支付，晚了就没有了哟！"));
             }
         }
     }
