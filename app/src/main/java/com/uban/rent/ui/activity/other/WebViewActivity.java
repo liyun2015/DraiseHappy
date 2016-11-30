09c394c1a08bd091017613c0c5ffba589abbc9ca
王波 <wangbo@uban.com>
2016-11-30 18:33:34 +0800

    ref

---
+++ app/src/main/java/com/uban/rent/ui/activity/other/WebViewActivity.java
@@ -0,0 +1,125 @@
+package com.uban.rent.ui.activity.other;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.uban.rent.R;
+import com.uban.rent.base.BaseActivity;
+
+import butterknife.Bind;
+
+public class WebViewActivity extends BaseActivity {
+    public static final String WEB_VIEW_URL = "url";
+    public static final String WEB_VIEW_DESC = "desc";
+
+    @Bind(R.id.toolbar_content_text)
+    TextView toolbarContentText;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.web_view)
+    WebView webView;
+    @Bind(R.id.activity_web_view)
+    LinearLayout activityWebView;
+
+    private String desc;
+    private String url;
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_web_view;
+    }
+
+    @Override
+    protected void afterCreate(Bundle savedInstanceState) {
+        initData();
+        //初始化
+        initView();
+    }
+    private void initData() {
+        Intent intent = getIntent();
+        url = intent.getStringExtra(WEB_VIEW_URL);
+        desc = intent.getStringExtra(WEB_VIEW_DESC);
+    }
+    private void initView() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+        toolbarContentText.setText(desc);
+        showLoadingView();
+        //WebView
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        webView.getSettings().setAppCacheEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress == 100){
+                    hideLoadingView();
+                }
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl(url);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (webView != null){
+            webView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (webView != null){
+            webView.onPause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (webView != null){
+            webView.removeAllViews();
+            webView.destroy();
+        }
+    }
+}
