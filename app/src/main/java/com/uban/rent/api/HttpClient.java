47c8a646a9cf88a81b3268d2b951a2c8d9321676
王波 <wangbo@uban.com>
2017-01-05 15:32:47 +0800

    ref

--- app/src/main/java/com/uban/rent/api/HttpClient.java
+++ app/src/main/java/com/uban/rent/api/HttpClient.java
@@ -3,15 +3,16 @@ package com.uban.rent.api;
 
 import com.uban.rent.App;
 import com.uban.rent.api.config.RequestInterceptor;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-
-import java.io.File;
-import java.util.concurrent.TimeUnit;
 
 /**
  * HttpClient
@@ -20,8 +21,8 @@ import java.util.concurrent.TimeUnit;
  */
 public class HttpClient {
 
-//    public static final String BASE_URL = "http://112.126.82.240:30104";
-    public static final String BASE_URL = "http://app.api.rest.uban.com:30104";
+    public static final String BASE_URL = "http://112.126.82.240:30104";
+//    public static final String BASE_URL = "http://app.api.rest.uban.com:30104";
     private static HttpClient mInstance;
     private Retrofit singleton;
 
